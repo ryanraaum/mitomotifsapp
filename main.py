31adfd6e522d9e5f47d6d450bec0dad339c5214f
Ryan Raaum <code@raaum.org>
2008-07-26 17:01:19 -0400

    too much to list

--- main.py
+++ main.py
@@ -24,11 +24,21 @@ import wsgiref.handlers
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
+from mitomotifs import sites2seq
 from mitomotifs import seq2sites
 from mitomotifs import sites2str
 from fasta import iterate_fasta
+from fasta import entry2str
 
-TEMPLATES = os.path.join(os.path.dirname(__file__), 'templates')
+MAX_SEQS_SHORT  = 50
+MAX_SEQS_LONG   = 20
+LONG_SEQ_CUTOFF = 1500
+TEMPLATES       = os.path.join(os.path.dirname(__file__), 'templates')
+
+class Result(object):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
 
 def _404error(handler):
     """Standard 404 actions for any RequestHandler."""
@@ -53,10 +63,12 @@ class Sites2SeqHandler(webapp.RequestHandler):
             _404error(self)
             
     def post(self, action):
+
         # get posted parameters
         format = self.request.get('format')
         output = self.request.get('output')
         content = self.request.get('content')
+
         # validate submission
         problems = []
         valid = True
@@ -66,47 +78,73 @@ class Sites2SeqHandler(webapp.RequestHandler):
         if not output in ['hvr1', 'hvr2', 'hvr1and2', 'hvr1to2', 'coding', 'all']:
             valid = False
             problems.append('given output not one of the acceptable options.')
-        content_lines = content.split('\n')
-        result_lines = []
+
         if valid:
+            content_lines = content.encode('utf8').split('\n')
+            names = []
+            ns = []
+            motifs = []
+            count = 0
             for curr_line in content_lines:
-                line = re.sub(r'[,;]', '', curr_line)
-                if format == 'motif_only':
-                    motifs = line
-                elif format == 'name_and_motif':
+                line = re.sub(r'[,;]', ' ', curr_line)
+
+                count += 1
+                name = 'Seq%s' % count
+                n = 1
+                motif = line
+                if format == 'name_and_motif':
                     split = line.split(' ', 1)
                     if len(split) == 2:
-                        name, motifs = split 
+                        name, motif = split 
                     else:
                         valid = False
                         problems.append('The entry "%s" does not seem to be correctly formatted' % curr_line)
-                else:
+                elif format == 'name_n_and_motif':
                     split = line.split(' ', 2)
                     if len(split) == 3:
-                        name, n, motifs = split 
+                        name, n, motif = split 
+                        if re.match(r'^[0-9]+$', n) is None:
+                            valid = False
+                            problems.append("One of the given 'N's is not a number")
+                        else:
+                            n = int(n)
                     else:
                         valid = False
                         problems.append('The entry "%s" does not seem to be correctly formatted' % curr_line)
-                try:
-                    result_lines.append(sites2seq(motifs.encode('utf8')))
-                except MitoMotifError, e:
-                    valid = False
-                    problems.append(e.message)
+                names.append(name)
+                ns.append(n)
+                motifs.append(motif)
             
+        if valid:
+            pnames = []
+            pseqs = []
+            for name,n,motif in zip(names,ns,motifs):
+                try:
+                    for i in range(n):
+                        pnames.append(name)
+                        pseqs.append(sites2seq(motif))
+                except Exception, e:
+                    valid = False
+                    problems.append(e)
+
         if not valid:
-            self.response.out.write('<h1>INVALID INPUT</h1>')
-            self.response.out.write('%s' % problems)
-        self.response.out.write('<p>action: %s</p>' % action)
-        self.response.out.write('<p>format: %s</p>' % cgi.escape(self.request.get('format')))
-        self.response.out.write('<p>output: %s</p>' % cgi.escape(self.request.get('output')))
-        self.response.out.write('<p>content: %s</p>' % cgi.escape(self.request.get('content')))
-        for entry in result_lines:
-            self.response.out.write('<p>result: %s</p>' % entry)
+            template_values = {'problems':problems}
+            path = os.path.join(TEMPLATES, 'standard_error.html')
+            self.response.out.write(template.render(path, template_values))
+        else:
+            as_fasta = ''.join(list(entry2str({'name':name,'sequence':seq}) for name,seq in zip(pnames, pseqs)))
+
+            template_values = {'format'     :format,
+                               'results'    :as_fasta}
+            path = os.path.join(TEMPLATES, 'sites2seq_result.html')
+            self.response.out.write(template.render(path, template_values))
 
 class Seq2SitesHandler(webapp.RequestHandler):
     def get(self, action):
         if action == "" or action == "main":
-            template_values = {}
+            template_values = {'max_long'   :MAX_SEQS_LONG,
+                               'max_short'  :MAX_SEQS_SHORT,
+                               'cutoff'     :LONG_SEQ_CUTOFF}
             path = os.path.join(TEMPLATES, 'seq2sites_main.html')
             self.response.out.write(template.render(path, template_values))
         else:
@@ -115,50 +153,78 @@ class Seq2SitesHandler(webapp.RequestHandler):
     def post(self, action):
 
         # get posted parameters
-        format = self.request.get('format')
-        seq_range = self.request.get('seq_range').encode('utf8')
         content = self.request.get('content')
 
-        # validate submission
+        # submission validation and error reporting
         problems = []
         valid = True
-        if not format in ['single_seq', 'fasta']:
-            valid = False
-            problems.append('given format not one of the acceptable options.')
-        if not seq_range in ['hvr1', 'hvr2', 'hvr1to2', 'coding', 'all']:
-            valid = False
-            problems.append('given sequence range not one of the acceptable options.')
 
-        # pull names and sequence out of submitted content
+        # get the submitted data
         content = content.encode('utf8')
+
+        # make sure something was submitted
+        if len(content) == 0:
+            valid = False
+            self.redirect("/seq2sites/")
+
+        # determine format
+        format = None
+        if content.startswith('>'):
+            format = 'fasta'
+        else:
+            format = 'single_seq'
+            
+        # pull names and sequence out of submitted content
         names = []
         seqs = []
         if format == 'fasta':
-            for entry in iterate_fasta(content, 'text'):
-                names.append(entry['name'])
-                seqs.append(entry['sequence'])
+            try:
+                fnames = []
+                fseqs = []
+                for entry in iterate_fasta(content, 'text'):
+                    fnames.append(entry['name'])
+                    fseqs.append(re.sub(r'[^ACGTURYMKSWBDHVN]', '', entry['sequence'].upper()))
+                names = fnames
+                seqs = fseqs
+            except:
+                valid = False
+                problems.append('There was an error in the FASTA format')
         else:
-            names = None
-            seqs = [content]
+            names = ['']
+            seqs = [re.sub(r'[^ACGTURYMKSWBDHVN]', '', content.upper())]
 
         # enforce limits for multisequence submissions
         if format == 'fasta':
-            pass
+            max_length = 0
+            for seq in seqs:
+                if len(seq) > max_length:
+                    max_length = len(seq)
+            if max_length <= LONG_SEQ_CUTOFF:
+                if len(seqs) > MAX_SEQS_SHORT:
+                    valid = False
+                    problems.append('too many sequences submitted')
+            elif len(seqs) > MAX_SEQS_LONG:
+                valid = False
+                problems.append('too many sequences submitted')
+
+        if not valid:
+            template_values = {'problems':problems}
+            path = os.path.join(TEMPLATES, 'standard_error.html')
+            self.response.out.write(template.render(path, template_values))
+            return self.response
 
         result_lines = []
         for seq in seqs:
-            sub = re.sub(r'[^ACGTURYMKSWBDHVN]', '', seq.upper())
-            result_lines.append(sites2str(seq2sites(sub, what=seq_range)))
+            try:
+                result_lines.append(sites2str(seq2sites(seq)))
+            except:
+                result_lines.append('There was an error processing this sequence')
 
-        if not valid:
-            self.response.out.write('<h1>INVALID INPUT</h1>')
-            self.response.out.write('%s' % problems)
-        self.response.out.write('<p>action: %s</p>' % action)
-        self.response.out.write('<p>format: %s</p>' % cgi.escape(self.request.get('format')))
-        self.response.out.write('<p>seq_range: %s</p>' % cgi.escape(self.request.get('seq_range')))
-        self.response.out.write('<p>content: %s</p>' % cgi.escape(self.request.get('content')))
-        for entry in result_lines:
-            self.response.out.write('<p>result: %s</p>' % entry)
+        results = list(Result(x,y) for x,y in zip(names,result_lines))
+        template_values = {'format'     :format,
+                           'results'    :results}
+        path = os.path.join(TEMPLATES, 'seq2sites_result.html')
+        self.response.out.write(template.render(path, template_values))
 
 class NoSuchURLHandler(webapp.RequestHandler):
     def get(self):
